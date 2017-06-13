a04d94bc654716bbd086a26b7443626330cbd0ca
andrey <andkr5150@gmail.com>
2017-06-13 23:42:34 +0300

    modify yml

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -5,10 +5,51 @@ namespace Tests\AppBundle\Controller;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Controller\DefaultController;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @var
+     */
+    public static $application;
+    /**
+     * @param $command
+     *
+     * @return int
+     */
+    public static function runAppConsoleCommand($command)
+    {
+        // $command = sprintf('%s --env=test', $command);
+        return self::getApplication()->run(new StringInput($command));
+    }
+    public static function setUpBeforeClass()
+    {
+        self::setUpMysql();
+    }
+    /**
+     * @return Application
+     */
+    public static function getApplication()
+    {
+        if (null === self::$application) {
+            self::$application = new Application(static::createClient()->getKernel());
+            self::$application->setAutoExit(false);
+        }
+        return self::$application;
+    }
+    /**
+     * @return void
+     */
+    public static function setUpMysql()
+    {
+        //  self::runAppConsoleCommand('doctrine:database:drop --force');
+        //  self::runAppConsoleCommand('doctrine:database:create');
+        //   self::runAppConsoleCommand('doctrine:schema:update --force --verbose=3');
+        //   self::runAppConsoleCommand('doctrine:fixtures:load -q --verbose=3');
+    }
+
     public function testindexAction()
     {
         $client = static::createClient(array(), array(
